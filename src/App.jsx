d09fa31122eb029e33b7118638ab9e62b58a8d53
AdityaPDEU <shrivastavaaditya8797@gmail.com>
2025-02-08 16:01:58 +0530

    update readme

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+// const count = 1
 
   return (
     <>
@@ -18,7 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 15)}>
           count is {count}
         </button>
         <p>
